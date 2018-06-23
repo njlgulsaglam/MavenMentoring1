4446cbdf8ff36a4f862f18e57b89a571c8235b82
njlgulsaglam <njlgulsaglam@gmail.com>
2018-06-22 23:03:20 -0400

    initial commit

---
+++ src/main/java/com/indeed/Calculator.java
@@ -0,0 +1,22 @@
+package com.indeed;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+	
+
+		System.out.println("This is testing indeed");
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.cnn.com");
+		
+	}
+
+}
